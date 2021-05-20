ac05467ee18e29be38b8a799308c21216cff7727
l-damyanov <l_l_damyanov@abv.bg>
2021-05-20 20:36:02 +0300

    Django URL's and Templates

--- DjangoWebBasics/settings.py
+++ DjangoWebBasics/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!*#ih1-9s44!&&+=d1+i*yw2^l%xcyo(#&ndi)706x-v(llv+u'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
